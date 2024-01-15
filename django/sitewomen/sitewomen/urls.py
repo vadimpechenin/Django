41cf007412fd47ed566461b60d0c818802aa567b
Vadim Pechenin <vadim.pechenin2015@gmail.com>
2024-01-15 14:26:31 +0400

    #4. Routing and presentation functions

--- django/sitewomen/sitewomen/urls.py
+++ django/sitewomen/sitewomen/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from women import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('women.urls')),
+    #можно сделать path('women/', include('women.urls')) тогда все пути будут #http://127.0.0.1:8000/women/ + суффикс из urls в women
 ]
---
+++ django/sitewomen/women/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index), #http://127.0.0.1:8000/
+    path('cats/', views.categories), #http://127.0.0.1:8000/cats/
+]

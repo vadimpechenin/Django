a9169920d20a051b04400bb5e9d5b56fe4eef320
vadimpechenin <vadim.pechenin2015@gmail.com>
2024-01-30 11:27:48 +0400

    #17. What is a database, SQL and ORM. Creating the first model

--- django/sitewomen/women/models.py
+++ django/sitewomen/women/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Women(models.Model):
+    title = models.CharField(max_length = 255)
+    content = models.TextField(blank=True) #можно оставлять пустым
+    time_create = models.DateTimeField(auto_now_add=True) #автоматически заполняется поле в момент появления записи
+    time_update = models.DateTimeField(auto_now=True) #при изменении записи меняется поле
+    is_published = models.BooleanField(default = True)
+

9e98fe17d43dbf12923f83a2410aad04721b0c39
vadimpechenin <vadim.pechenin2015@gmail.com>
2024-02-07 20:57:16 +0400

    #29. Adding tags to the site

--- django/sitewomen/women/templatetags/women_tags.py
+++ django/sitewomen/women/templatetags/women_tags.py
@@ -1,6 +1,6 @@
 from django import template
 import women.views as views
-from women.models import Category
+from women.models import Category, TagPost
 
 register = template.Library()
 
@@ -10,3 +10,8 @@ register = template.Library()
 def show_categories(cat_selected=0):
     cats = Category.objects.all()
     return {'cats': cats, 'cat_selected': cat_selected}
+
+
+@register.inclusion_tag('women/list_tags.html')
+def show_all_tags():
+    return {'tags': TagPost.objects.all()}
--- django/sitewomen/women/views.py
+++ django/sitewomen/women/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify #импорт фильтров для страницы
 
-from women.models import Women, Category
+from women.models import Women, Category, TagPost
 
 menu = [
     {'title': 'О сайте', 'url_name': 'about'},
@@ -74,6 +74,18 @@ def show_category(request, cat_slug):
             'menu': menu,
             'posts': posts,
             'cat_selected': category.pk,
+            }
+    return render(request, 'women/index.html', context = data)
+
+def show_tag_post_list(request, tag_slug):
+    tag = get_object_or_404(TagPost, slug = tag_slug)
+    posts = tag.tags.filter(is_published=Women.Status.PUBLISHED)
+
+    data = {
+            'title': f'Тег: {tag.tag}',
+            'menu': menu,
+            'posts': posts,
+            'cat_selected': None,
             }
     return render(request, 'women/index.html', context = data)
 

551619359ac0cc364fbbf357c465c2464acf180f
vadimpechenin <vadim.pechenin2015@gmail.com>
2024-02-15 11:22:57 +0400

    #39. Custom fields and actions in the admin panel

--- django/sitewomen/women/admin.py
+++ django/sitewomen/women/admin.py
@@ -1,15 +1,30 @@
-from django.contrib import admin
-
+from django.contrib import admin, messages
 from women.models import Women, Category
 
 
 @admin.register(Women)  #аналог admin.site.register(Women, WomenAdmin)
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published', 'cat') # поля для отображения в таблице
-    list_display_links = ('id', 'title',) # какие поля позволяют перейти к описанию объекта
+    list_display = ('title', 'time_create', 'is_published', 'cat','brief_info') # поля для отображения в таблице
+    list_display_links = ('title',) # какие поля позволяют перейти к описанию объекта
     ordering = ['-time_create', 'title'] #сортировка по полям
     list_editable = ('is_published',) #Кортеж полей для изменения
     list_per_page = 5 #Максимальное количество записей для отображения
+    actions=['set_published','set_draft'] #Список задаваемых действий
+
+    @admin.display(description="Краткое описание", ordering="content")
+    def brief_info(self, women: Women):
+        #Метод для пользовательского поля, которого нет в БД
+        return f"Описание {len(women.content)} символов."
+
+    @admin.action(description="Опубликовать выбранные записи")
+    def set_published(self, request, queryset):
+        count= queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f"Изменено {count} записей.")
+
+    @admin.action(description="Стять с публикации выбранные записи")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f"{count} записей снято с публикации!", messages.WARNING) #WARNING формирует значек
 
 
 @admin.register(Category)

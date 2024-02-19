d64408a0e8a29a001df30893f04b2dd41f71988e
vadimpechenin <vadim.pechenin2015@gmail.com>
2024-02-19 11:00:14 +0400

    #40. Search bar and filter panel

--- django/sitewomen/women/admin.py
+++ django/sitewomen/women/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin, messages
 from women.models import Women, Category
 
 
+class MarriedFilter(admin.SimpleListFilter):
+    title = "Статус женщин"
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('married', 'Замужем'),
+            ('single', 'Не замужем')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'maried':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
 @admin.register(Women)  #аналог admin.site.register(Women, WomenAdmin)
 class WomenAdmin(admin.ModelAdmin):
     list_display = ('title', 'time_create', 'is_published', 'cat','brief_info') # поля для отображения в таблице
@@ -10,6 +26,8 @@ class WomenAdmin(admin.ModelAdmin):
     list_editable = ('is_published',) #Кортеж полей для изменения
     list_per_page = 5 #Максимальное количество записей для отображения
     actions=['set_published','set_draft'] #Список задаваемых действий
+    search_fields = ['title__startswith','cat__name'] #Дополнительная панель поиск
+    list_filter = [MarriedFilter, 'cat__name', 'is_published'] #поля, по которым будем делать фильтрацию
 
     @admin.display(description="Краткое описание", ordering="content")
     def brief_info(self, women: Women):

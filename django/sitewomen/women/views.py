9ab72e540a5ae7f568264a7bfc4ad7ead16fdf18
vadimpechenin <vadim.pechenin2015@gmail.com>
2024-02-21 11:34:58 +0400

    #43. What are HTML forms. Sending data via GET and POST requests

--- django/sitewomen/women/views.py
+++ django/sitewomen/women/views.py
@@ -47,7 +47,7 @@ def show_post(request, post_slug):
 
 
 def addpage(request):
-    return HttpResponse("Добавление статьи")
+    return render(request, 'women/addpage.html', {'menu': menu, 'title': 'Добавление статьи'})
 
 
 def contact(request):

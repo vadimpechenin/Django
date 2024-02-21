f87e0ab77c463f34f96b0b400034ef6a2e002c9f
vadimpechenin <vadim.pechenin2015@gmail.com>
2024-02-21 11:05:40 +0400

    #42. Customizing the appearance of the admin panel

--- django/sitewomen/sitewomen/settings.py
+++ django/sitewomen/sitewomen/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 #STATICFILES_DIRS = [ BASE_DIR / 'sitewomen/static'] #Если css и js в базовом проекте
 # Default primary key field type
